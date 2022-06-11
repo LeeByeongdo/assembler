9b5d42c9cc1946b1be938cab920edd185a3cd8fc
byeongdolee <bdbdbdlee@gmail.com>
2022-06-11 18:50:22 +0900

    change name

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let input_file_path = assembler::parse_input_file(&args).unwrap_or_else(|err| {
+    let input_file_path = ntt_assembler::parse_input_file(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    assembler::run(input_file_path);
+    ntt_assembler::run(input_file_path);
 }
