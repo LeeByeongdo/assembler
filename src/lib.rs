33187fed10039841c7fb76e087b0c89ceda6085b
byeongdolee <bdbdbdlee@gmail.com>
2022-06-11 17:25:49 +0900

    assembler complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,22 @@
 extern crate core;
 
+use std::collections::HashMap;
 use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Lines};
 use std::iter::Peekable;
 use std::ops::Add;
 
-pub fn parse_input_file(args: &[String]) -> Result<&str, &'static str> {
+pub fn parse_input_file(args: &[String]) -> Result<String, &'static str> {
     if args.len() < 2 {
         return Err("not enough arguments");
     }
 
-    Ok(&args[1])
+    Ok(String::from(&args[1]))
 }
 
-pub fn run(file: &str) {
-    let file = File::open(file).unwrap();
-    let lines = BufReader::new(file).lines();
-    let peekable = lines.peekable();
-
-    let mut parser = Parser {
-        lines: peekable,
-        current_command: None,
-    };
+pub fn run(file: String) {
+    let mut parser = Parser::new(file);
 
     parser.parse();
 }
@@ -32,27 +26,86 @@ enum CommandType {
     ACommand,
     CCommand,
     LCommand,
+    IGNORE,
 }
 
 struct Parser {
-    pub lines: Peekable<Lines<BufReader<File>>>,
-    pub current_command: Option<String>,
+    file_path: String,
+    lines: Peekable<Lines<BufReader<File>>>,
+    current_command: Option<String>,
+    variable_address: usize,
 }
 
 impl Parser {
+    fn new(file_path: String) -> Parser {
+        let file = File::open(file_path.as_str()).unwrap();
+        let buf_reader = BufReader::new(file);
+
+        Parser {
+            file_path,
+            lines: buf_reader.lines().peekable(),
+            current_command: None,
+            variable_address: 16,
+        }
+    }
+
     fn parse(&mut self) {
+        self.variable_address = 16;
+
         let mut result = String::new();
+
+        let mut symbol_table = SymbolTable::new();
+
+        while self.has_more_commands() {
+            self.advance();
+            let command_type = self.command_type();
+
+            match command_type {
+                CommandType::ACommand | CommandType::CCommand => {
+                    symbol_table.command_address_counter += 1;
+                }
+                CommandType::LCommand => {
+                    let command = self.current_command.clone().unwrap();
+                    symbol_table.table.insert(
+                        String::from(&command[1..command.len() - 1]),
+                        symbol_table.command_address_counter,
+                    );
+                }
+                CommandType::IGNORE => {}
+            }
+        }
+
+        let file = File::open(self.file_path.as_str()).unwrap();
+        let buf_reader = BufReader::new(file);
+        self.lines = buf_reader.lines().peekable();
 
         while self.has_more_commands() {
             self.advance();
 
             let command_type = self.command_type();
-
             match command_type {
                 CommandType::ACommand => {
-                    let address = i16::from_str_radix(self.symbol().as_str(), 10).unwrap();
-                    let f = format!("{address:016b}");
-                    result = result.add(f.as_str());
+                    let address = usize::from_str_radix(self.symbol().as_str(), 10);
+                    let addr = match address {
+                        Ok(number_address) => {
+                            number_address
+                        }
+                        Err(_) => {
+                            let symbol = self.symbol();
+                            let addr = if symbol_table.contains(&symbol) {
+                                symbol_table.get_address(&symbol)
+                            } else {
+                                let variable_address = self.variable_address;
+                                symbol_table.add_entry(String::from(symbol), variable_address);
+                                self.variable_address += 1;
+                                variable_address
+                            };
+                            addr
+                        }
+                    };
+
+                    let formatted = format!("{addr:016b}");
+                    result = result.add(formatted.as_str());
                 }
                 CommandType::CCommand => {
                     let dest = self.dest();
@@ -68,9 +121,17 @@ impl Parser {
                     result = result.add(f.as_str());
                 }
                 CommandType::LCommand => {}
+                _ => {}
             }
 
-            result = result.add("\n");
+            match command_type {
+                CommandType::ACommand | CommandType::CCommand => {
+                    result = result.add("\n");
+                }
+                _ => {}
+            }
+
+
         }
 
         fs::write("program.hack", result).unwrap();
@@ -83,7 +144,16 @@ impl Parser {
     fn advance(&mut self) {
         if self.has_more_commands() {
             let command = self.lines.next().unwrap().unwrap();
-            self.current_command = Some(command);
+            let index = command.find("//");
+
+            match index {
+                None => {
+                    self.current_command = Some(command);
+                }
+                Some(idx) => {
+                    self.current_command = Some(String::from(&command[0..idx]));
+                }
+            }
         }
     }
 
@@ -92,11 +162,15 @@ impl Parser {
             panic!("command error");
         }
 
-        let command = String::from(self.current_command.as_ref().unwrap());
+        let command = String::from(self.current_command.as_ref().unwrap().trim());
+
+        if command.is_empty() {
+            return CommandType::IGNORE;
+        }
 
         let first_char = command.chars().nth(0);
         if let None = first_char {
-            panic!("starting line char error");
+            panic!("starting line char error: {}", command);
         }
 
         let first_char = first_char.unwrap();
@@ -104,7 +178,12 @@ impl Parser {
         match first_char {
             '@' => CommandType::ACommand,
             '(' => CommandType::LCommand,
-            _ => CommandType::CCommand,
+            _ => {
+                if command[0..2].eq("//") {
+                    return CommandType::IGNORE;
+                }
+                CommandType::CCommand
+            }
         }
     }
 
@@ -117,7 +196,7 @@ impl Parser {
             panic!("command error");
         }
 
-        let command = String::from(self.current_command.as_ref().unwrap());
+        let command = String::from(self.current_command.as_ref().unwrap().trim());
         String::from(command.get(1..).unwrap())
     }
 
@@ -242,12 +321,64 @@ impl Code {
             "M-1" => "1110010",
             "D+M" | "M+D" => "1000010",
             "D-M" => "1010011",
-            "M-D" => "10000111",
+            "M-D" => "1000111",
             "D&M" | "M&D" => "1000000",
             "D|M" | "M|D" => "1010101",
             &_ => {
-                panic!("bad comp input")
+                panic!("bad comp input: {}", comp);
             }
         }
+    }
+}
+
+#[derive(Debug)]
+struct SymbolTable {
+    table: HashMap<String, usize>,
+    command_address_counter: usize,
+}
+
+impl SymbolTable {
+    fn new() -> SymbolTable {
+        let mut map = HashMap::new();
+        map.insert(String::from("SP"), 0);
+        map.insert(String::from("LCL"), 1);
+        map.insert(String::from("ARG"), 2);
+        map.insert(String::from("THIS"), 3);
+        map.insert(String::from("THAT"), 4);
+        map.insert(String::from("R0"), 0);
+        map.insert(String::from("R1"), 1);
+        map.insert(String::from("R2"), 2);
+        map.insert(String::from("R3"), 3);
+        map.insert(String::from("R4"), 4);
+        map.insert(String::from("R5"), 5);
+        map.insert(String::from("R6"), 6);
+        map.insert(String::from("R7"), 7);
+        map.insert(String::from("R8"), 8);
+        map.insert(String::from("R9"), 9);
+        map.insert(String::from("R10"), 10);
+        map.insert(String::from("R11"), 11);
+        map.insert(String::from("R12"), 12);
+        map.insert(String::from("R13"), 13);
+        map.insert(String::from("R14"), 14);
+        map.insert(String::from("R15"), 15);
+        map.insert(String::from("SCREEN"), 16384);
+        map.insert(String::from("KBD"), 24576);
+
+        SymbolTable {
+            table: map,
+            command_address_counter: 0,
+        }
+    }
+
+    fn add_entry(&mut self, symbol: String, address: usize) {
+        self.table.insert(symbol, address);
+    }
+
+    fn contains(&self, symbol: &str) -> bool {
+        self.table.contains_key(symbol)
+    }
+
+    fn get_address(&self, symbol: &str) -> usize {
+        *self.table.get(symbol).unwrap()
     }
 }
